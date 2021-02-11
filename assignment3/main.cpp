2f2c55b85f92999d832d80c2b12a608d060f9b50
Ben <benlee1218@gmail.com>
2021-02-10 20:27:55 -0800

    classes

--- assignment3/main.cpp
+++ assignment3/main.cpp
@@ -3,35 +3,52 @@
 #include <stdio.h>
 #include <vector>
 #include <bitset>
+
 using namespace std;
 
+class Record{
+	public:
+		float eid;
+		string name;
+		string bio;
+		float mid;
+		int hash_value;
+};
+
 class Block{
-	
-}
+	// float size = 5;
+	public:
+		int block_number;
+		vector <Record> Primary;
+		vector <Record> Overflow;
+};
+
+
 
 class LinearHash{
 	public:
 		int num_entries = 0;
 		int num_buckets = 0;
 		int bucket_cap = 5;
-}
+		// vector <Block*, > Buckets;
+};
 
 
 
 int main(int argc, char *argv[]){
 	//creation mode
-	if (argv[1] == 'C'){
-		FILE * pFile;
-		  pFile = fopen ("Employees.csv","r");
-		  if (pFile!=NULL)
-		  {
-		    while(fscanf(pFile))
-		  }
-	}
-	//search mode
-	else if(argv[1] == 'L'){
-		target_id = stoi(argv[2])
+	// if (argv[1] == "C"){
+	// 	FILE * pFile;
+	// 	  pFile = fopen ("Employees.csv","r");
+	// 	  if (pFile!=NULL)
+	// 	  {
+	// 	    while(fscanf(pFile))
+	// 	  }
+	// }
+	// //search mode
+	// else if(argv[1] == 'L'){
+	// 	target_id = stoi(argv[2])
 
-	}
+	// }
 	return 0;
 }
