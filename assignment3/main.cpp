e09e81dc508ebeddf00d38fd4260756c3d18700f
mmamel <mma0671@gmail.com>
2021-02-12 21:30:09 -0800

    added more io stuff

--- assignment3/main.cpp
+++ assignment3/main.cpp
@@ -5,6 +5,7 @@
 #include <bitset>
 #include <unordered_map>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
@@ -48,13 +49,21 @@ bool CapOk(int num_rec, int N){
 
 void addBlock(){
 	ofstream fp;
-	fp.open("test.txt");
+	fstream file("test.txt");
+	fp.open("test.dat", ios::binary);
 	string temp("11432121,Michell Haney,Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.,11432122");
     string temp2("11432113,Kory Born,Quis commodo odio aenean sed adipiscing diam donec adipiscing. Scelerisque purus semper eget duis at tellus at. Cras pulvinar mattis nunc sed blandit libero volutpat sed. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Sapien pellentesque habitant morbi tristique senectus et. Mauris in aliquam sem fringilla. Amet mauris commodo quis imperdiet massa tincidunt. Accumsan sit amet nulla facilisi morbi tempus. Pellentesque habitant morbi tristique senectus et.,11432114");
     temp.resize(4096, ' ');
     temp2.resize(4096, ' ');
-	fp << temp;
-	fp << temp2;
+    char char_array[4097];
+    char char_array2[4097];
+    strcpy(char_array2, temp2.c_str());
+    strcpy(char_array, temp.c_str());
+	file.seekp(4096*3);
+	file.write(char_array, 4096);
+	file.write(char_array2, 4096);
+	// fp << temp;
+	// fp << temp2;
 	fp.close();
 }
 int readNumber(ifstream &fp){
@@ -65,7 +74,7 @@ int readNumber(ifstream &fp){
 string readBlock(int offset){
 	ifstream fp;
 	fp.open("test.txt");
-	char block[4096];
+	char block[4097];
     block[4096] = 0;
     fp.seekg(4096 * offset, ios::beg);
     fp.read((char *)&block, 4096);
@@ -73,29 +82,29 @@ string readBlock(int offset){
     string str(block);
     return str;
 }
-void readBucket(){
-	ifstream fp;
-	fp.open("bucket.txt");
-	// string num_record_str;
-	// std::getline(fp, num_record_str);
-	// int num_record = stoi(num_record_str);
-	int num_record = readNumber(fp);
-	// string N_str;
-	// std::getline(fp, N_str);
-	// int N = stoi(N_str);
-	int N = readNumber(fp);
-	cout << "num records " << num_record << endl;
-	for(int i = 0;i < N; i++){
-		cout << i<<endl;
-	}
-	if(!CapOk){
-		cout << "need a split" << endl;
-	}
-	else{
-		cout << "no split needed" << endl;
-	}
-	fp.close();
-}
+// void readBucket(){
+// 	ifstream fp;
+// 	fp.open("bucket.dat", ios::binary);
+// 	// string num_record_str;
+// 	// std::getline(fp, num_record_str);
+// 	// int num_record = stoi(num_record_str);
+// 	int num_record = readNumber(fp);
+// 	// string N_str;
+// 	// std::getline(fp, N_str);
+// 	// int N = stoi(N_str);
+// 	int N = readNumber(fp);
+// 	cout << "num records " << num_record << endl;
+// 	for(int i = 0;i < N; i++){
+// 		cout << i<<endl;
+// 	}
+// 	if(!CapOk){
+// 		cout << "need a split" << endl;
+// 	}
+// 	else{
+// 		cout << "no split needed" << endl;
+// 	}
+// 	fp.close();
+// }
 // [ [1] [2,3]   ]
 void createBucketArray(vector<vector<int>> &vect){
 	ifstream fp;
@@ -119,6 +128,7 @@ void createBucketArray(vector<vector<int>> &vect){
 			vect.at(i).push_back(overflow_offset);
 		}
 	}
+	fp.close();
 }
 void printBucketArray(vector<vector<int>> vect){
 	for(int i =0; i<vect.size();i++){
@@ -146,8 +156,63 @@ void buildIndex(vector<string> &vect, int N){
 	}
 
 }
+
+void incrementRecordCt(){
+	ifstream ifile;
+	ofstream ofile;
+	ifile.open("bucket.txt");
+	// fstream file("bucket.txt", ios::out | ios::in);
+	string remade_str;
+	string str;
+	std::getline(ifile, str);
+	int ct = stoi(str);
+	ct++;
+	remade_str = std::to_string(ct);
+	remade_str += '\n';
+	while(std::getline(ifile, str)){
+		remade_str = remade_str + str+'\n';
+	}
+
+	ifile.close();
+	ofile.open("bucket.txt");
+
+	int string_length = remade_str.length();
+	char char_array[string_length+1];
+    strcpy(char_array, remade_str.c_str());
+    char_array[string_length] = 0;
+    ofile << remade_str;
+	ofile.close();
+}
+void incrementBucketCt(){
+	ifstream ifile;
+	ofstream ofile;
+	ifile.open("bucket.txt");
+	// fstream file("bucket.txt", ios::out | ios::in);
+	string remade_str;
+	string str;
+	std::getline(ifile, str);
+	remade_str = str + '\n';
+	std::getline(ifile, str);
+	int ct = stoi(str);
+	ct++;
+	str = std::to_string(ct);
+	remade_str += str + '\n';
+	while(std::getline(ifile, str)){
+		remade_str = remade_str + str+'\n';
+	}
+
+	ifile.close();
+	ofile.open("bucket.txt");
+
+	int string_length = remade_str.length();
+	char char_array[string_length+1];
+    strcpy(char_array, remade_str.c_str());
+    char_array[string_length] = 0;
+    ofile << remade_str;
+	ofile.close();
+}
 int main(int argc, char *argv[]){
-	// addBlock();
+	addBlock();
 	vector<string> index;
 	index.push_back("0");
 	index.push_back("1");
@@ -156,10 +221,11 @@ int main(int argc, char *argv[]){
 	printBucketArray(bucketArray);
 	buildIndex(index, 7);
 	for(int i=0;i<index.size();i++){
-		ints bits = index.at(i).length();
-		string currkey = levelKey( bits )
-		currkey = index.at(i);
+	
 
 		// cout << index[i] << " ";
 	}
+	cout << readBlock(0);
+	incrementRecordCt();
+	incrementBucketCt();
 }
